ed93434fab3948240c43e07ee0c5e72c4df029c3
LuciferTM <roy_qian@gmail.com>
2017-05-23 20:34:27 +0800

    spittleController

---
+++ src/main/java/com/lucifer/spitter/data/SpittleRepository.java
@@ -0,0 +1,15 @@
+package com.lucifer.spitter.data;
+
+import com.lucifer.spitter.Spittle;
+import java.util.List;
+
+public interface SpittleRepository {
+    /**
+     * @param max
+     *            待返回的最大的Spittle ID
+     * @param count
+     *            返回Spittle对象的个数
+     * @return
+     */
+    List<Spittle> findSpittles(long max, int count);
+}
