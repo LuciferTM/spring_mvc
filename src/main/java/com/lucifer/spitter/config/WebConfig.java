925740c084f3ac0d9f22a07490b0a0ae26db53e8
LuciferTM <roy_qian@gmail.com>
2017-05-24 13:59:10 +0800

    修改WebConfig以支持JSTL标签

--- src/main/java/com/lucifer/spitter/config/WebConfig.java
+++ src/main/java/com/lucifer/spitter/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
+import org.springframework.web.servlet.view.JstlView;
 
 
 @Configuration
@@ -21,9 +21,18 @@ public class WebConfig extends WebMvcConfigurerAdapter{
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
+//        当JSP页面使用JSTL标签时，就需要配置InternalResourceViewResolver来解析JstlView了。
+//        JSTL格式化标签需要一个Locale来正确地格式化一些特定语言环境的值，如日期和币种。消息标签可以使用Spring消息源和Locale来正确地选中消息并解析到HTML。
+//        要解析JstlView，需要在InternalResourceViewResolver中设置viewClass属性：
+        resolver.setViewClass(JstlView.class);
         return resolver;
     }
 
+//XML设置方式
+//    <bean id="viewResolver"
+//    class="org.springframework.web.servlet.view.InternalResourceViewResolver"
+//    p:prefix="/WEB-INF/views/"
+//    p:suffix=".jsp" />
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
         configurer.enable();
